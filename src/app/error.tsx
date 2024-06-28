bb2b801300491f79986923c7b2c2a5d7d535cead
shu♡ <72540000+xxx-44@users.noreply.github.com>
2024-06-28 23:02:46 +0900

    Update error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,17 +1,17 @@
-'use client';
-import Link from 'next/link';
-import React from 'react';
+// 'use client';
+// import Link from 'next/link';
+// import React from 'react';
 
-const ErrorPage = () => {
-	return (
-		<div className='h-screen flex flex-col justify-center items-center bg-sky-50 text-gray-900'>
-			<h1 className='text-8xl font-bold'>ERROR</h1>
-			<p className='text-4xl font-medium'>Unxepected error occurred</p>
-			<Link href='/' className='mt-4 text-xl text-blue-600 hover:underline'>
-				Back to Home
-			</Link>
-		</div>
-	);
-};
+// const ErrorPage = () => {
+// 	return (
+// 		<div className='h-screen flex flex-col justify-center items-center bg-sky-50 text-gray-900'>
+// 			<h1 className='text-8xl font-bold'>ERROR</h1>
+// 			<p className='text-4xl font-medium'>Unxepected error occurred</p>
+// 			<Link href='/' className='mt-4 text-xl text-blue-600 hover:underline'>
+// 				Back to Home
+// 			</Link>
+// 		</div>
+// 	);
+// };
 
-export default ErrorPage;
+// export default ErrorPage;
