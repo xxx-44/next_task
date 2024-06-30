f43163a3cf4cff93265cd402260cbf36b4a418c9
shu <shu.l.iove.you31@gmail.com>
2024-06-30 21:23:17 +0900

    add task fix

--- src/app/(main)/expired/page.tsx
+++ src/app/(main)/expired/page.tsx
@@ -1,6 +1,8 @@
 import TaskCard from '@/component/TaskCard/TaskCard';
 import { TaskDocument } from '@/models/task';
+import Link from 'next/link';
 import React from 'react';
+import { MdAddTask } from 'react-icons/md';
 
 const page = async () => {
 	const getExpiredTasks = async (): Promise<TaskDocument[]> => {
@@ -19,6 +21,14 @@ const page = async () => {
 		<div className='text-gray-800 p-8 h-full overflow-y-auto pb-24'>
 			<header className='flex justify-between items-center'>
 				<h1 className='text-2xl font-bold items-center'>Expired Tasks</h1>
+
+				<Link
+					href='/new'
+					className='flex items-center gap-1 font-semibold border px-4 py-2 rounded-full shadow-sm text-white bg-gray-800 hover:bg-gray-500'
+				>
+					<MdAddTask className='size-5' />
+					<div>Add Task</div>
+				</Link>
 			</header>
 			<div className='mt-8 flex flex-wrap gap-4'>
 				{expiredTasks.map((task) => (
